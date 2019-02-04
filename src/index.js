0513ea039db6478fd9f6b9a404c01f5f54f564ce
Josep López <jayden.haylie@gmail.com>
2019-02-03 22:58:06 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.scss'
+
+// const form = document.forms[0]
+// const fName = form.children[0].children[1]
+// const email = form.children[1].children[1]
+// const password = form.children[2].children[1]
+
+// const validation = {
+//   name_error: false,
+//   email_error: false,
+//   password_error: false,
+// }
+
+// const errorStyles = {
+//   color: 'red',
+//   solid: 'solid',
+//   pixels: '2px',
+// }
+
+// if (fName.value.length < 0) {
+//   validation.name_error = true
+// }
+
+// if (email.value.length < 0) {
+//   validation.email_error = true
+// }
+
+// if (password.value.length < 3) {
+//   validation.password_error = true
+// }
+
+// const checkValidation = (name) => {
+//   const { name_error, email_error, password_error } = validation
+//   if (name_error === true || email_error === true || password_error == true) {
+//     console.log(name)
+//   }
+// }
+
+// form.addEventListener('change', (e) => checkValidation(e.target.name))
